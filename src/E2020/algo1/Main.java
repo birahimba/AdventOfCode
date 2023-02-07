97766103b438167b909512c9ccfa84a52b3c103d
Elhadj BA <elhadj.ba.ext@geodis.com>
2023-02-07 23:59:22 +0100

    result

--- src/E2020/algo1/Main.java
+++ src/E2020/algo1/Main.java
@@ -2,18 +2,32 @@ package E2020.algo1;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main{
     
     public static void main(String[] args) throws FileNotFoundException {
+        int targetNum = 2020;
+        List<Integer> nums = new ArrayList<>();
 
-        // lecture du fichier
-        File input  = new File("src/E2020/algo1/input.txt");
-        Scanner obj = new Scanner(input);
-        while (obj.hasNextLine())
-            System.out.println(obj.nextLine());
-    }
+        Scanner sc = new Scanner(new File("src/E2020/algo1/input.txt"));
+        //on ajoute nos num dans le input
+        while (sc.hasNextInt()) {
+            nums.add(sc.nextInt());
+        }
 
+        //on sort la liste
+        Collections.sort(nums);
+
+        for (int i = 0; i < nums.size(); i++) {
+            int difference = targetNum - nums.get(i);
+            int index = Collections.binarySearch(nums, difference);
+            if (index > 0 && index != i) {
+                int result = difference * nums.get(i);
+                System.out.println(result);
+                break;
+            }
 
         }
+    }
+}

06cd727a70028d23cd5bc0eb5568849179154618
Elhadj BA <elhadj.ba.ext@geodis.com>
2023-02-07 12:43:16 +0100

    structure

---
+++ src/E2020/algo1/Main.java
@@ -0,0 +1,10 @@
+package E2020.algo1;
+
+public class Main{
+    
+    public static void main(String[] args){
+        // TODO document why this method is empty
+        System.out.println("hello");
+    }
+    
+        }
